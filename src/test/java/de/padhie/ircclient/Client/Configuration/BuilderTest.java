2cef652ff979adc974ce5f51718364b01bf9ceac
padhie <padhiez+gitlab@hotmail.de>
2024-04-09 20:34:32 +0200

    fix tests

--- src/test/java/de/padhie/ircclient/Client/Configuration/BuilderTest.java
+++ src/test/java/de/padhie/ircclient/Client/Configuration/BuilderTest.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
 
 public class BuilderTest {
     @Test
